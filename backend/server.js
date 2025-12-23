0d78d31dd2f9577b8e1f1e15dfc5dcb3a0c9c378
Damaris <damarishi@edu.aau.at>
2025-12-23 17:36:42 +0100

    added route for login on backend

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 
+const loginRoutes = require("./routes/login");
+
+app.use('/login', loginRoutes);
+
 app.get("/", (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send('Test API server running');
