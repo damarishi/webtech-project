82a60505c8d3e70f41da69ed432afc1898d4412c
Damaris <damarishi@edu.aau.at>
2025-12-23 17:23:15 +0100

    Backend and Frontend initialized and tested

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send('Test API server running');
 });
