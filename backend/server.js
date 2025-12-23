7a43603449475023eb8dc0ae5287076a3d77a0ea
Damaris <damarishi@edu.aau.at>
2025-12-23 17:04:07 +0100

    Added server.js file

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send('Test API server running');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
