9a499120522897e54bc30818e9909f728ba54964
Damaris <damarishi@edu.aau.at>
2026-01-09 15:49:01 +0100

    Basic functionality for user frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//import user components
+import { User } from './pages/user/user';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' }, //redirect to main page when created
+  {
+    path: 'user',
+    component: User,
+    //canActivate: [AuthGuard]
+  },
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('frontend');
-  name = 'foodora clone';
+  name = 'EatNow';
 }
