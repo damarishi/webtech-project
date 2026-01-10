f3de5af4aee74c3b33e6b2f4a118459fdf418dbc
Damaris <damarishi@edu.aau.at>
2026-01-10 14:11:52 +0100

    styled loading screen

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './shared/ui/navbar/navbar';
+import {Loading} from './shared/ui/loading/loading';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Loading],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
