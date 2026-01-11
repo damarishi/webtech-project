c8cb3c72dcb6693954b14f7674ac872629673366
Leonardo Tocci <leonardotocci6@gmail.com>
2026-01-11 10:52:00 +0100

    WIP

---
+++ frontend/src/app/pages/site-manager/site-manager.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SiteManager } from './site-manager';
+
+describe('SiteManager', () => {
+  let component: SiteManager;
+  let fixture: ComponentFixture<SiteManager>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SiteManager]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SiteManager);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
