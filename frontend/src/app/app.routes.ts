3cbcffb7d2ab66dc613abaa2cd15be1c09a6a9fb
Leonardo Tocci <leonardotocci6@gmail.com>
2026-01-21 23:29:27 +0100

    Frontend: separate route files, changed route role data - exported routes for each role to own file - each route now only supports one role that is checked on redirects

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -12,10 +12,11 @@ import {RestaurantOwner} from './pages/restaurant-owner/restaurant-owner';
 import {USER_ROUTES} from './pages/user/user.routes';
 import {User} from './pages/user/user';
 import {UserRoles} from './types/user-roles';
+import {OWNER_ROUTES} from './pages/restaurant-owner/owner.routes';
 
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' }, //redirect to main page when created
+  { path: '',redirectTo: 'login', pathMatch: 'full'  }, //redirect to main page when created
 
   {
     path: 'login',
@@ -33,27 +34,28 @@ export const routes: Routes = [
       path: 'site-manager',
       component: SiteManager,
       canActivate: [AuthGuard],
-    data: { roles: [UserRoles.ADMIN] },
+      data: { role: UserRoles.ADMIN },
   },
 
   {
       path: 'site-manager/data-view/:category',
       component: DataView,
       canActivate: [AuthGuard],
-      data: { roles: [UserRoles.ADMIN] },
+      data: { role: UserRoles.ADMIN },
   },
 
   {
     path: 'restaurant-owner',
     component: RestaurantOwner,
+    children: OWNER_ROUTES,
     canActivate: [AuthGuard],
-    data: { roles: [UserRoles.OWNER] },
+    data: { role: UserRoles.OWNER },
   },
   {
     path: 'user',
     component: User,
     children: USER_ROUTES,
     canActivate: [AuthGuard],
-    data: { roles: [UserRoles.USER] },
+    data: { role: UserRoles.USER },
   }
 ];
