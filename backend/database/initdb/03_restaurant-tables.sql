f4b704b23a4ee3f88bc7786060a03d8bda71aabc
Leonardo Tocci <leonardotocci6@gmail.com>
2026-01-24 20:02:16 +0100

    DB: Added Status to orders table

--- backend/database/initdb/03_restaurant-tables.sql
+++ backend/database/initdb/03_restaurant-tables.sql
@@ -118,6 +118,7 @@ CREATE TABLE orders
     user_id         INT NOT NULL,
     total           DECIMAL(10,2) NOT NULL,
     discount_id     INT NOT NULL,
+    status          INT NOT NULL,
     fee             INT NOT NULL,
     date            TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (restaurant_id) REFERENCES restaurant(restaurant_id),
